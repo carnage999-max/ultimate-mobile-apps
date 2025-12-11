0d98fff6af075f9e8f28a8d7d51e1e8dc00203e8
11011 <jamesezekiel039@gmail.com>
2025-12-11 16:52:17 +0100

    Remove AWS_REGION environment variable, default to us-east-1

--- lambda/send-email/index.js
+++ lambda/send-email/index.js
@@ -1,7 +1,8 @@
 // Lambda function to send emails via AWS SES
 const AWS = require('aws-sdk');
 
-const ses = new AWS.SES({ region: process.env.AWS_REGION || 'us-east-1' });
+// Default to us-east-1 (AWS Lambda automatically sets the region, but we'll default to us-east-1)
+const ses = new AWS.SES({ region: 'us-east-1' });
 
 exports.handler = async (event) => {
   // CORS headers
